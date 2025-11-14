fe3e721aa40487d1aae76efef5bb4d16e99d20a3
omkar-4 <omkar.pro04@gmail.com>
2025-11-14 21:15:10 +0530

    adding update manifest for testing

--- resources/js/checkForUpdates.js
+++ resources/js/checkForUpdates.js
@@ -1,6 +1,17 @@
 try {
-  let url = "";
+  let url = "https://";
   let manifest = await Neutralino.updater.checkForUpdates(url);
+
+  if (manifest.version != NL_APPVERSION) {
+    await Neutralino.updater.install();
+    await Neutralino.app.restartProcess();
+  } else {
+    console.log('You are using the latest version!');
+  }
 } catch (err) {
   // handle error
+  console.error("error updating the app", err);
+  console.error(err.code, err.message);
+  
+
 }
